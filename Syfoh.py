b601fdebb660eb4400350516569c3bb7a6e283a3
Max Zuidberg <m.zuidberg@icloud.com>
2021-08-21 21:36:44 +0200

    Fix: command without linebreak parsed incorrectly

--- Syfoh.py
+++ Syfoh.py
@@ -220,7 +220,7 @@ if __name__ == "__main__":
     cmds = []
     if p.is_file():
         with open(p) as f:
-            cmds = [cmd[:-1] for cmd in f.readlines()]
+            cmds = [cmd.rstrip("\n") for cmd in f.readlines()]
     else:
         cmds.append(args.input)
 
